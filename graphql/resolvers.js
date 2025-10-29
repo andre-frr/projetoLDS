fc00d866c78dc850db5f38d381b9457fb9eb8d31
André Ferreira <220001208@esg.ipsantarem.pt>
2025-10-29 10:09:16 +0000

    Refactor PostgreSQL connection to use DATABASE_URL environment variable

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,11 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export const resolvers = {
