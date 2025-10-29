9c33400de61cb49d6d567d5e350f9975a83d2894
André Ferreira <220001208@esg.ipsantarem.pt>
2025-10-29 12:16:44 +0000

    Add gRPC services for service-a and service-b with PostgreSQL integration

---
+++ grpc/service-a/server.js
@@ -0,0 +1,37 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: "postgres://user:password@db:5432/database",
+});
+
+const PROTO_PATH = '../protos/data.proto';
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+const data_proto = grpc.loadPackageDefinition(packageDefinition).data;
+
+function getData(call, callback) {
+  // For now, just return a dummy response
+  // In the future, this will query the database
+  callback(null, {data: "Data for " + call.request.id});
+}
+
+function main() {
+  const server = new grpc.Server();
+  server.addService(data_proto.Data.service, {getData: getData});
+  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+    console.log('Server running at http://0.0.0.0:50051');
+  });
+}
+
+main();
+
---
+++ grpc/service-b/client.js
@@ -0,0 +1,29 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = '../protos/data.proto';
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+const data_proto = grpc.loadPackageDefinition(packageDefinition).data;
+
+function main() {
+  const client = new data_proto.Data('localhost:50051',
+                                       grpc.credentials.createInsecure());
+  client.getData({id: '1'}, function(err, response) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Data:', response.data);
+  });
+}
+
+main();
+
