39f800ed8a660cc5e41282be7b85f91031b160a3
André Ferreira <220001208@esg.ipsantarem.pt>
2025-10-30 09:29:58 +0000

    Add Express server to handle gRPC data requests

--- grpc/service-b/client.js
+++ grpc/service-b/client.js
@@ -1,5 +1,6 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+const express = require('express');
 
 const PROTO_PATH = './protos/data.proto';
 
@@ -14,17 +15,32 @@ const packageDefinition = protoLoader.loadSync(
     });
 const data_proto = grpc.loadPackageDefinition(packageDefinition).data;
 
-function main() {
+function main(callback) {
     const client = new data_proto.Data('service-a:50051',
         grpc.credentials.createInsecure());
     client.getData({id: '1'}, function (err, response) {
         if (err) {
             console.error(err);
+            callback(err, null);
             return;
         }
         console.log('Data:', response.data);
+        callback(null, response.data);
     });
 }
 
-main();
+const app = express();
+const port = 3001;
 
+app.get('/', (req, res) => {
+    main((err, data) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Service-b listening at http://localhost:${port}`);
+});
