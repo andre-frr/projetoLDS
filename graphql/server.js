fe90397121ad47c87b14effda1ba2b81baed1e6c
André Ferreira <220001208@esg.ipsantarem.pt>
2025-11-04 10:01:11 +0000

    Update dependencies and refactor server setup for improved performance

--- graphql/server.js
+++ graphql/server.js
@@ -1,19 +1,25 @@
-import express from "express";
-import {ApolloServer} from '@apollo/server';
-import {expressMiddleware} from '@apollo/server/express4';
-import dotenv from "dotenv";
-import {typeDefs} from "./schema.js";
-import {resolvers} from "./resolvers.js";
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@as-integrations/express5';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { typeDefs } from './schema.js';
+import { resolvers } from './resolvers.js';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 await server.start();
 
-app.use("/graphql", expressMiddleware(server));
+app.use(
+    '/graphql',
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server)
+);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
