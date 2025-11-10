8e6910f8ed487f69b4d5ddc019d53a9c2f8f6b77
André Ferreira <220001208@esg.ipsantarem.pt>
2025-11-10 09:50:22 +0000

    Hotfix: Update server.js to set Next.js directory to './pages'

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev, dir: '.'});
+const app = next({dev, dir: './pages'});
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
